1a8d7b0c2248d6d5adce72f2b4f7c12d31587739
Radu <avadanii@tiscali.it>
2021-02-28 11:43:41 +0200

    some minor changes

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <div>
+      <h1>I'm footer</h1>
+    </div>
+  )
+}
---
+++ src/pages/blog.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export const blog = () => {
+  return <Layout>hello from blog page!!!</Layout>
+}
