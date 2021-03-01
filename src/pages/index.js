9f7e0841912eafd9c1d2a90286e0b927a7ac8d02
Radu <avadanii@tiscali.it>
2021-03-01 21:18:59 +0200

    some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
+import { Link } from "gatsby"
 import React from "react"
+import Banner from "../components/Banner"
 import Layout from "../components/Layout"
-// import { FaAdobe } from "react-icons/fa"
+import SimpleHero from "../components/SimpleHero"
 
 export default function Home() {
   return (
     <Layout>
-      <div>Hello World!!!</div>
+      <SimpleHero>
+        <Banner
+          title="continue exploring"
+          info="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, molestiae?"
+        >
+          <Link to="/tours" className="btn-white">
+            explore tours
+          </Link>
+        </Banner>
+      </SimpleHero>
     </Layout>
   )
 }
